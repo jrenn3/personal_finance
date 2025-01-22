22c48693aa73698d6a0b1386a5454db2416f67c9
Joseph Renner <jrenn3@gmail.com>
2025-01-21 22:07:09 -0600

    release notes page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+        input: {
+            main: './index.html', // Main page
+            releaseNotes: './src/pages/release-notes.html', // Additional page
+        },
+    },
   },
 });
