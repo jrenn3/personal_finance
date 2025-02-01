1151db19ad7f7d8ce16ce0ca19f7e760ec987285
jrenn3 <jrenn3@gmail.com>
2025-02-01 11:00:16 -0600

    configuring auto emails

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
 const {onValueCreated} = require("firebase-functions/v2/database");
 const nodemailer = require("nodemailer");
 
-const myEmail = "jr.auto.generated.emails@gmail.com";
+const myEmail = "excelbymoe@gmail.com";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: myEmail,
-    pass: "ayfl whbs chyg zxaa",
+    pass: "jebkax-gerfez-4xeKwi",
   },
 });
 
@@ -16,9 +16,32 @@ exports.sendEmailOnDatabaseWrite = onValueCreated("/downloads/{pushId}", async (
   const mailOptions = {
     from: myEmail,
     to: "jmren3@gmail.com, marissamhemmings@gmail.com",
-    subject: `New FUNds download by ${data.name}`,
+    subject: `New Excel By Moe downloaded by ${data.name}`,
     text: `
             Name: ${data.name}
+            Email: ${data.email}
+            Source: ${data.source}
+            Model: ${data.modelName}
+            Timestamp: ${data.timestamp}
+        `,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+});
+
+exports.sendEmailOnDatabaseWrite = onValueCreated("/downloads/{pushId}", async (event) => {
+  const data = event.data.val(); // Access new database entry
+  const mailOptions = {
+    from: myEmail,
+    to: data.email,
+    subject: `Enjoy your new template!`,
+    text: `
+            Thank you ${data.name} for your download. If you have any questions or feedback, please don't hesitate to reach out to us at ${myEmail}.
             Email: ${data.email}
             Source: ${data.source}
             Model: ${data.modelName}
