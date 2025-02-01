4e8e16ac2d2e7e42af8aecf4d3904378da54644d
jrenn3 <jrenn3@gmail.com>
2025-02-01 12:08:54 -0600

    configure two email functions

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,50 @@
 const {onValueCreated} = require("firebase-functions/v2/database");
 const nodemailer = require("nodemailer");
 
-const myEmail = "jr.auto.generated.emails@gmail.com";
+const myEmail = "excelbymoe@gmail.com";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: myEmail,
-    pass: "ayfl whbs chyg zxaa",
+    pass: "paav cfeg ejuh qlrx",
   },
 });
 
 exports.sendEmailOnDatabaseWrite = onValueCreated("/downloads/{pushId}", async (event) => {
   const data = event.data.val(); // Access new database entry
-  const mailOptions = {
+  const ownerMailOptions = {
     from: myEmail,
     to: "jmren3@gmail.com, marissamhemmings@gmail.com",
-    subject: `New Excel By Moe downloaded by ${data.name}`,
-    text: `
-            Name: ${data.name}
-            Email: ${data.email}
-            Source: ${data.source}
-            Model: ${data.modelName}
-            Timestamp: ${data.timestamp}
-        `,
+    subject: `New downloaded by ${data.name}`,
+    text: `Name: ${data.name}
+Email: ${data.email}
+Source: ${data.source}
+Model: ${data.modelName}
+Timestamp: ${data.timestamp}
+    `,
+  };
+  const userMailOptions = {
+    from: myEmail,
+    to: data.email,
+    subject: `Enjoy your new template!`,
+    text: `Thank you ${data.name} for your ${data.modelName} download.
+
+If you have any questions or feedback, please don't hesitate to reach out to us at excelbymoe@gmail.com.
+
+Marissa & Joe (aka Moe)
+    `,
   };
 
   try {
-    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(ownerMailOptions);
     console.log("Email sent successfully!");
   } catch (error) {
     console.error("Error sending email:", error);
   }
-});
-
-exports.sendEmailOnDatabaseWrite = onValueCreated("/downloads/{pushId}", async (event) => {
-  const data = event.data.val(); // Access new database entry
-  const mailOptions = {
-    from: myEmail,
-    to: data.email,
-    subject: `Enjoy your new template!`,
-    text: `
-            Thank you ${data.name} for your ${data.modelName} download.
-            
-            If you have any questions or feedback, please don't hesitate to reach out to us at excelbymoe@gmail.com.
-        `,
-  };
 
   try {
-    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(userMailOptions);
     console.log("Email sent successfully!");
   } catch (error) {
     console.error("Error sending email:", error);
