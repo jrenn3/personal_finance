da36e7f84616256bd342a60a20b7674a65ee1300
jrenn3 <jrenn3@gmail.com>
2025-02-14 17:20:17 -0600

    link clean-up

--- dist/assets/render-DMFQwkUR.js
+++ dist/assets/render-DMFQwkUR.js
@@ -1,4 +1,4 @@
-(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const o=""+new URL("window_NTM-Cgl8hPLy.png",import.meta.url).href,s=""+new URL("window_Recurring-De4AyuFB.png",import.meta.url).href,d=""+new URL("window_Balances _ Actions-BO1o5T99.png",import.meta.url).href;function c(){const r=window.location.pathname,i=r.includes("index.html")?"#":"/personal_finance/index.html",a=r.includes("more.html")?"#":"/personal_finance/src/pages/more.html",n=r.includes("release-notes.html")?"#":"/personal_finance/src/pages/release-notes.html";return`
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const l="/personal_finance/assets/window_NTM-Cgl8hPLy.png",o="/personal_finance/assets/window_Recurring-De4AyuFB.png",c="/personal_finance/assets/window_Balances%20_%20Actions-BO1o5T99.png";function d(){const a=window.location.pathname,n=a.includes("index.html")?"#":"/personal_finance/index.html",r=a.includes("more.html")?"#":"/personal_finance/src/pages/more.html",i=a.includes("release-notes.html")?"#":"/personal_finance/src/pages/release-notes.html";return`
         <header id="header" class="fixed top-0 left-0 w-full bg-white z-50 shadow-sm transition-all duration-300">
             <div class="container mx-auto flex justify-center items-center px-6 py-4">
                 <div class="logo mr-auto">
@@ -6,9 +6,9 @@
                 </div> 
                 <!-- Navigation Menu -->
                 <nav class="md:flex space-x-8">
-                    <a href="${i}" class="text-neutral-dark hover:text-primary">Home</a>
-                    <a href="${a}" class="text-neutral-dark hover:text-primary">More</a>
-                    <a href="${n}" class="text-neutral-dark hover:text-primary">Release Notes</a>
+                    <a href="${n}" class="text-neutral-dark hover:text-primary">Home</a>
+                    <a href="${r}" class="text-neutral-dark hover:text-primary">More</a>
+                    <a href="${i}" class="text-neutral-dark hover:text-primary">Release Notes</a>
                 </nav>
             </div>
         </header>
@@ -25,7 +25,7 @@
             </div>
             <p>&copy; 2025 By Moe. All Rights Reserved. Not financial advice.</p>
         </footer>
-    `}function m(){return`
+    `}function f(){return`
         <div id="userModal" class="hidden fixed inset-0 bg-neutral-dark bg-opacity-50 flex items-center justify-center">
             <div class="bg-white rounded-lg p-6 w-full max-w-sm">
                 <h2 class="text-xl font-bold mb-4 text-primary">Enter Your Details</h2>
@@ -59,21 +59,21 @@
                 </form>
             </div>
         </div>
-    `}function f(r,i,a,n,e,t){return`
-        <section id="${r}" class="min-h-screen flex items-center justify-center bg-neutral-light relative mt-20">
+    `}function m(a,n,r,i,e,t){return`
+        <section id="${a}" class="min-h-screen flex items-center justify-center bg-neutral-light relative mt-20">
             <div class="container mx-auto flex flex-col lg:flex-row items-center px-8 space-y-12 lg:space-y-0">
                 
                 <!-- Left Side: Zoomed-in Excel Screenshot -->
-                <div id="${r}-image" class="w-full lg:w-1/2 flex justify-center transform transition-all">
-                    <img src="${n}" alt="${e}" class="w-full max-w-lg shadow-lg rounded-lg">
+                <div id="${a}-image" class="w-full lg:w-1/2 flex justify-center transform transition-all">
+                    <img src="${i}" alt="${e}" class="w-full max-w-lg shadow-lg rounded-lg">
                 </div>
 
                 <!-- Right Side: Feature Callout -->
                 <div class="w-full lg:w-1/2">
-                    <h2 id="${r}-headline" class="text-4xl font-bold text-primary transition-all lg:ml-6">
-                        ${i}
+                    <h2 id="${a}-headline" class="text-4xl font-bold text-primary transition-all lg:ml-6">
+                        ${n}
                     </h2>
-                    <div id="${r}-callout" class="my-6 text-xl transform md:translate-y-10">
+                    <div id="${a}-callout" class="my-6 text-xl transform md:translate-y-10">
                         <p class="bg-white shadow-md rounded-lg p-6 lg:ml-6">
                             ${t}
                         </p>
@@ -81,4 +81,4 @@
                 </div>
             </div>
         </section>
-    `}function p(){document.body.insertAdjacentHTML("afterbegin",c()),document.body.insertAdjacentHTML("beforeend",u()),document.body.insertAdjacentHTML("beforeend",m());const r=[{id:"scene2",headline:"The FUNds Forecast is... future focused",subHeadline:"",imgSrc:o,imgAlt:"Excel NTM Tab",calloutText:"Sick of logging past transactions? Don’t. Review where you want to go, not where you’ve been."},{id:"scene3",headline:"The FUNds Forecast is... customizable",subHeadline:"",imgSrc:s,imgAlt:"Excel NTM Tab",calloutText:"Allows adaptation to fit your personal situation."},{id:"scene4",headline:"The FUNds Forecast is... action oriented",subHeadline:"",imgSrc:d,imgAlt:"Excel NTM Tab",calloutText:"Actions matter, not visuals. We minimize distracting, redundant graphics and show you what you need."},{id:"scene5",headline:"The FUNds Forecast is... not a b*dget",subHeadline:"",imgSrc:o,imgAlt:"Excel NTM Tab",calloutText:"We're all scared of budgeting because it means limiting yourself. Instead, give yourself the comfort to SPEND IT WELL."}],i=document.getElementById("scene1");r.forEach(a=>{i.insertAdjacentHTML("afterend",f(a.id,a.headline,a.subHeadline,a.imgSrc,a.imgAlt,a.calloutText))})}p();
+    `}function p(){document.body.insertAdjacentHTML("afterbegin",d()),document.body.insertAdjacentHTML("beforeend",u()),document.body.insertAdjacentHTML("beforeend",f());const a=[{id:"scene2",headline:"The FUNds Forecast is... future focused",subHeadline:"",imgSrc:l,imgAlt:"Excel NTM Tab",calloutText:"Sick of logging past transactions? Don’t. Review where you want to go, not where you’ve been."},{id:"scene3",headline:"The FUNds Forecast is... customizable",subHeadline:"",imgSrc:o,imgAlt:"Excel NTM Tab",calloutText:"Allows adaptation to fit your personal situation."},{id:"scene4",headline:"The FUNds Forecast is... action oriented",subHeadline:"",imgSrc:c,imgAlt:"Excel NTM Tab",calloutText:"Actions matter, not visuals. We minimize distracting, redundant graphics and show you what you need."},{id:"scene5",headline:"The FUNds Forecast is... not a b*dget",subHeadline:"",imgSrc:l,imgAlt:"Excel NTM Tab",calloutText:"We're all scared of budgeting because it means limiting yourself. Instead, give yourself the comfort to SPEND IT WELL."}],n=document.getElementById("scene1");a.forEach(r=>{n.insertAdjacentHTML("afterend",m(r.id,r.headline,r.subHeadline,r.imgSrc,r.imgAlt,r.calloutText))})}p();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/personal_finance/',
   build: {
     rollupOptions: {
         input: {
