7f90d445b79378007f5d90442b390d8045827357
jrenn3 <jrenn3@gmail.com>
2025-02-16 16:42:37 -0600

    update recepit file URL

--- src/more.js
+++ src/more.js
@@ -2,7 +2,7 @@ import './styles/styles.css';
 import { setupModal, showDownloadPopup, setupShowModalBtns, setupDownloadCount } from './common';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const modelURL = 'files/TEMPLATE_ReceiptSplitter_ByMoe_v5.xlsx';
+    const modelURL = '../../files/TEMPLATE_ReceiptSplitter_ByMoe_v5.xlsx';
     const showModalBtns = document.querySelectorAll('.showModalBtn-Receipt');
     const downloadButton = document.querySelector('.downloadButton'); // Updated selector
 
