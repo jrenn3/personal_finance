7d0ebfcf1d5e6121caa5d55cbab35521b716565a
jrenn3 <jrenn3@gmail.com>
2025-02-16 16:27:11 -0600

    whitelist Render domain

--- vite.config.js
+++ vite.config.js
@@ -19,5 +19,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
+    allowedHosts: ["excelbymoe.onrender.com"], // Whitelist your Render domain
   },
 });
